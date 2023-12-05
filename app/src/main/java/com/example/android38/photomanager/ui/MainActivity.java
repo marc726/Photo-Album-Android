05f78b001ed38dbbe0e4135c4e88d94aa8eea913
marc726 <marcrizzolo726@gmail.com>
2023-12-04 22:27:34 -0500

    Barebones

--- app/src/main/java/com/example/android38/photomanager/ui/MainActivity.java
+++ app/src/main/java/com/example/android38/photomanager/ui/MainActivity.java
@@ -1,26 +1,28 @@
 package com.example.android38.photomanager.ui;
 
 import android.os.Bundle;
-import android.view.View;
 import android.view.Menu;
-
-import com.example.android38.R;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
+import android.view.MenuItem;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.android38.R;
 import com.example.android38.databinding.ActivityMainBinding;
+import com.example.android38.photomanager.adapter.AlbumAdapter;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+    private AlbumAdapter albumAdapter; // Placeholder for your adapter
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,17 +32,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
+
+        // Setup RecyclerView with a dummy adapter to display albums
+        RecyclerView recyclerView = binding.appBarMain.contentMain.recyclerView;
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        albumAdapter = new AlbumAdapter(); // Initialize with actual data
+        recyclerView.setAdapter(albumAdapter);
+
+        // Replace with your own action, for example, create a new album
+        binding.appBarMain.fab.setOnClickListener(view -> {
+            // TODO: Implement the logic to create a new album
+            // This may involve starting a new Activity or DialogFragment
+            Snackbar.make(view, "FAB Clicked", Snackbar.LENGTH_LONG).show();
         });
+
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
                 .setOpenableLayout(drawer)
@@ -52,9 +59,19 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        // Handle action bar item clicks here.
+        if (id == R.id.action_settings) {
+            // Handle settings action
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
